114492b1bc197aae07a7a53d15696dd05964ed08
Wes Williams <wwwestech@gmail.com>
2014-08-02 12:10:55 -0600

    reorganized scripts directory

--- passport-config.js
+++ passport-config.js
@@ -3,16 +3,21 @@ var commonConfig = require('./common-config.js');
 var appConfig = {};
 appConfig._host = commonConfig.host;
 
-appConfig.fitbit   = require('./scripts/third/fitbit.js')('KEY_GOES_HERE','PASS_GOES_HERE');
-appConfig.twitter  = require('./scripts/third/twitter.js')('KEY_GOES_HERE','PASS_GOES_HERE');
-appConfig.github   = require('./scripts/third/github.js')('KEY_GOES_HERE','PASS_GOES_HERE');
-appConfig.pearson  = require('./scripts/third/pearson.js')('KEY_GOES_HERE');
+// Business APIs
+appConfig.fitbit   = require('./scripts/apis/biz/fitbit.js')('KEY_GOES_HERE','PASS_GOES_HERE');
+appConfig.twitter  = require('./scripts/apis/biz/twitter.js')('KEY_GOES_HERE','PASS_GOES_HERE');
+appConfig.github   = require('./scripts/apis/biz/github.js')('KEY_GOES_HERE','PASS_GOES_HERE');
+appConfig.pearson  = require('./scripts/apis/biz/pearson.js')('KEY_GOES_HERE');
 
-appConfig.datagov  = require('./scripts/us/data-gov.js')('KEY_GOES_HERE');
-appConfig.openco   = require('./scripts/us/co/opencolorado.js')('KEY_GOES_HERE');
-appConfig.cim      = require('./scripts/us/co/cim.js')('KEY_GOES_HERE');
+// Government APIs
+appConfig.datagov  = require('./scripts/apis/gov/usa/data-gov.js')('KEY_GOES_HERE');
+appConfig.openco   = require('./scripts/apis/gov/usa/co/opencolorado.js')('KEY_GOES_HERE');
+appConfig.cim      = require('./scripts/apis/gov/usa/co/cim.js')('KEY_GOES_HERE');
 
+// Mashups
+appConfig.mashup1  = require('./scripts/apis/mash/template.js')();
+
+// Webhooks
 appConfig.webhook1 = require('./scripts/hooks/template.js')();
-appConfig.mashup1  = require('./scripts/mashups/template.js')();
 
 module.exports = appConfig;
