ff83aaaa79f2a95b9da17a7cd367a1b20c17af26
Wes Williams <wwwestech@gmail.com>
2014-08-03 07:52:12 -0600

    added hack for littlebits until apps can be created to get oauth creds

--- scripts/apis/biz/littlebits.js
+++ scripts/apis/biz/littlebits.js
@@ -1,7 +1,9 @@
 // Littlebits API
 // http://developer.littlebitscloud.cc/api-http
+/* Can't make an application yet
 module.exports = function(idValue, secretValue) { 
   var config = {};
+  config.name='Littlebits';
   config.enabled=true;
   config.type='oauth-2.0';
   config.authorizationUrl = 'https://littlebits.cc/oauth/authorize';
@@ -11,5 +13,19 @@ module.exports = function(idValue, secretValue) {
   config.userIdPath = '0.user_id'; 
   config.clientId = idValue;
   config.clientSecret = secretValue;
+  return config;
+};
+*/
+
+// but we have access to the bearer token 
+module.exports = function(keyValue) {
+  var config = {};
+  config.name='Littlebits';
+  config.enabled=true;
+  config.type='api-key';
+  config.baseUrl = 'https://api-http.littlebitscloud.cc'; 
+  config.keyName = 'Authorization';
+  config.keyValue = 'Bearer ' + keyValue;
+  config.isKeyInUrl = false;
   return config;
 };
